2f8b3fea1687800fe16a78925f92f7188502f161
SaiKeshav <keshavkolluru@gmail.com>
2022-06-02 16:09:02 +0530

    added installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+pip install t5
+pip install ipdb
+pip install --upgrade gdown
+pip install overrides
+pip install pytorch-lightning==0.7.6
+pip install scikit-learn==0.23.1
+pip install torchtext==0.5.0
+pip install transformers==2.6.0
+pip install tokenizers==0.5.2
+
+# import nltk
+# nltk.download('stopwords')
